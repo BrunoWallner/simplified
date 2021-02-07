9869e5bfdc1d68289ee7a9a4dd57d24919c775c6
luca <lucab1211@gmail.com>
2021-02-07 12:26:58 +0100

    added date support

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,8 @@ use std::io::stdin;
 use rand::thread_rng;
 use rand::Rng;
 
+use chrono::prelude::*;
+
 pub fn sleep(millis: u64) {
     let duration = time::Duration::from_millis(millis);
     thread::sleep(duration);
@@ -20,6 +22,11 @@ pub fn input() -> String {
 
 pub fn random(number_1: i32, number_2: i32) -> i32 {
     let mut rng = thread_rng();
-    return rng.gen_range(number_1..number_2);
+    let final: i32 = rng.gen_range(number_1..number_2);
+    return final;
 }
 
+pub fn date() {
+    let utc: DateTime<Utc> = Utc::now();
+    return utc;
+}
